dc42439fed7d6cbb255035fefe4a679adde5940a
26440 <2644001371@qq.com>
2026-09-21 21:46:42 +0800

    v1

--- main.c
+++ main.c
@@ -4,4 +4,8 @@ int main()
 {
     // @TODO: print a sentence you want.
     printf("Hello, world!\n");
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
 }
